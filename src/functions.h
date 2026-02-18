5b465c2daa8b3974c208e4ffeeb5097f52f561a3
litnps <lalitth21.n@gmail.com>
2026-02-18 17:27:50 +0700

    update functions.h : include headers

--- src/functions.h
+++ src/functions.h
@@ -1,45 +1,62 @@
 #pragma once
-#include<bits/stdc++.h>
+#include<iostream>
+#include<tuple>
+#include<string>
+#include<vector>
+#include<ctime>
 
 using namespace std;
 
 class Event{
 private:
     string title;
-
-    // ? : how to store an event time ?? using tuple or else (does it have anything easier mai krub)
-    tuple<int,int,int,int> E_start, E_end; // Year month date time
+    time_t E_start, E_end;
+    string category, details, places;
+    unsigned long long id;
     
-    string category, details, places;    
     // note: can add more if want to
+
+    // compare func for sort
+    // sort by start date, end date, id
+    // bool operator<(const Event &a) const {
+    //     if(E_start != a.E_start) return E_start < a.E_start;
+    //     if(E_end != a.E_end) return E_end < a.E_end;
+    //     return id < a.id;
+    // }
 
 public:
 //todo : write all func 
 
-    void create(); // to do : write  input
-    
+    Event(string t = "", time_t s = 0, time_t e = 0, string c = "Default", 
+            string d = "", string p = "", unsigned long long i = 0){
+        title = t;
+        E_start = s;
+        E_end = e;
+        category = c;
+        details = d;
+        places = p;
+        id = i;
+    }
+   
     // ? : use operator to know what edit or just write all func
     void editString(char, string); 
-    void editTime(char, tuple<int,int,int,int>);
+    void editTime(char, time_t);
 
     //return private variables
     string getTitle(){ return title; }
-    tuple<int,int,int,int> getStartTime(){ return E_start; }
-    tuple<int,int,int,int> getStartTime(){ return E_end; }
+    time_t getStartTime(){ return E_start; }
+    time_t getEndTime(){ return E_end; }
     string getCategory(){ return category; }
     string getDetails(){ return details; }
     string getPlaces(){ return places; }
 };
 
-void Event::create(){
-    
-}
 
 void Event::editString(char op , string temp){
     // ? : do like this mai krub
     
     if(op == 't'){ // edit title
-        // ? : recheck word 
+        // ? : recheck word before edit, like-- does it have '|' 
         title = temp;
     }
     else if(op){
@@ -49,7 +66,7 @@ void Event::editString(char op , string temp){
     return;
 }
 
-void Event::editTime(char, tuple<int,int,int,int>){
+void Event::editTime(char op, time_t newTime){
     // edit what is not strings
 }
 
@@ -59,16 +76,9 @@ void Event::editTime(char, tuple<int,int,int,int>){
 vector<Event> showWhatsNext(int); 
 // show first n Events that upcoming
 
-vector<Event> showthisdate(); 
-// show Events on this date 
-// ? : what should I input na
-
-vector<Event> showthisRange();
-// show Events on these range 
-// ? : what should I input na
-
-// ?? : can func 2 and 3 group together
+vector<Event> showthisRange(time_t, time_t); 
+// in case u want 2 find a date, use time start and time end on that date to find e_start of every event
 
 Event searchFor(string);
-// show event from serch
+// show event from search
 // ? : return with pointer mai?
