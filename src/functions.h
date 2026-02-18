305b9609b0772b4c9c81ec601d08f0b65d25a4ce
litnps <lalitth21.n@gmail.com>
2026-02-18 22:36:08 +0700

    update function.h : add Class CalendarManager

--- src/functions.h
+++ src/functions.h
@@ -4,6 +4,7 @@
 #include<string>
 #include<vector>
 #include<ctime>
+#include<algorithm>
 
 using namespace std;
 
@@ -18,15 +19,13 @@ private:
 
     // compare func for sort
     // sort by start date, end date, id
-    // bool operator<(const Event &a) const {
-    //     if(E_start != a.E_start) return E_start < a.E_start;
-    //     if(E_end != a.E_end) return E_end < a.E_end;
-    //     return id < a.id;
-    // }
+     bool operator<(const Event &a) const {
+         if(E_start != a.E_start) return E_start < a.E_start;
+         if(E_end != a.E_end) return E_end < a.E_end;
+         return id < a.id;
+     }
 
 public:
-//todo : write all func 
-
     Event(string t = "", time_t s = 0, time_t e = 0, string c = "Default", 
             string d = "", string p = "", unsigned long long i = 0){
         title = t;
@@ -37,10 +36,6 @@ public:
         places = p;
         id = i;
     }
-   
-    // ? : use operator to know what edit or just write all func
-    void editString(char, string); 
-    void editTime(char, time_t);
 
     //return private variables
     string getTitle(){ return title; }
@@ -51,34 +46,87 @@ public:
     string getPlaces(){ return places; }
 };
 
-
-void Event::editString(char op , string temp){
-    // ? : do like this mai krub
+class CalendarManager {
+private:
+    vector<Event> allEvents; // contains all events
+    string filename = "calendar_data.txt"; // save all events in this file na / can change name na krub
     
-    if(op == 't'){ // edit title
-        // ? : recheck word before edit, like-- does it have '|' 
-        title = temp;
-    }
-    else if(op){
-        // bla bla bla
-    }
+    void sortEvents(); //function to sort all events (by date)
+    unsigned long long ID = 1; // ID for each events, update every time we add new one 
 
-    return;
+public:
+    CalendarManager(); // auto load
+    ~CalendarManager(); // auto save
+
+    //file I/O
+    void saveToFile();
+    void loadFromFile();
+
+    //core logic
+    bool addEvent(Event newEvent); // return true if add sucess
+    bool deleteEvent(int eventId); // return true if delete sucsees
+    bool editEvent(int eventId, Event updatedEvent); // return true if edit sucsess
+
+    // Find & Search
+    const vector<Event>& getAllEvents() const; // ใช้ const& ไม่ให้ copy ข้อมูล เปลืองแรม
+    vector<Event> getEventsByDate(int day, int month, int year);
+    vector<Event> searchEvents(string keyword);
+    vector<Event> getUpcomingEvents(int N); // next N events 
+};
+
+// functions definitions
+
+CalendarManager::CalendarManager() {loadFromFile();}
+CalendarManager::~CalendarManager() {saveToFile();}
+
+void CalendarManager::loadFromFile() {
+    // todo : recieve all data from filename to vector<Event>
+    
+    // ? : if we already have data should we delete first ?
 }
 
-void Event::editTime(char op, time_t newTime){
-    // edit what is not strings
+void CalendarManager::saveToFile() {
+    // todo : put all data from vector<Event>  to filename (aka. save functions)
 }
 
-// ------------------------------------------------
-// all others func.
+void CalendarManager::sortEvents() {
+    sort(allEvents.begin(), allEvents.end());
+}
 
-vector<Event> showWhatsNext(int); 
-// show first n Events that upcoming
+bool CalendarManager::addEvent(Event newEvent){
+    //todo : add newEvent to allEvents
+    //todo : check if the date is correct 
+    
+    //add event then sort
+    //return true if sucsess
+}
+bool CalendarManager::deleteEvent(int eventId){
+    //todo : delete event by look for this eventID
+    
+    //return true if sucsess
+}
+bool CalendarManager::editEvent(int eventId, Event updatedEvent) {
+    //todo : look for this eventId and update
+    //if the date is change then sort (or just sort every time)
+    //return true if sucsess
+}
 
-vector<Event> showthisRange(time_t, time_t); 
-// in case u want 2 find a date, use time start and time end on that date to find e_start of every event
+const vector<Event>& CalendarManager::getAllEvents() const {
+    return allEvents;
+}
 
-Event searchFor(string);
-// show event from search
-// ? : return with pointer mai?
+vector<Event> CalendarManager::getEventsByDate(int day, int month, int year) {
+    //todo : getallEvent on Date
+    //maybe using lowerbound
+}
+
+vector<Event> CalendarManager::searchEvents(string keyword) {
+    //todo : search for events by keyword  
+}
+
+vector<Event> CalendarManager::getUpcomingEvents(int N) {
+    vector<Event> upcoming;
+    int n = min(N, (int)allEvents.size());
+    for (int i = 0; i < n ; ++i) upcoming.push_back(allEvents[i]);
+    return upcoming;
+}
